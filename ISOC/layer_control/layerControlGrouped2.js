f5ca0390875cfa39fe381d6efcaa087acdf7b35c
Tunchz <Tunchz@gmail.com>
2020-11-05 22:45:28 +0700

    Update layerControlGrouped2.js

--- ISOC/layer_control/layerControlGrouped2.js
+++ ISOC/layer_control/layerControlGrouped2.js
@@ -18,7 +18,7 @@ class layerControlGrouped {
     this._collapsed = false;
 
     if (options.options && options.options.collapsed) {
-      //this._collapsed = true;
+      this._collapsed = true;
     }
 
     this._layers = options.layers.reverse().slice()
