eb81f63910582587f666d53fdb15d8a319205420
Tunchz <Tunchz@gmail.com>
2020-11-18 18:27:03 +0700

    Update layerControlGrouped2.js

--- ISOC/layer_control/layerControlGrouped2.js
+++ ISOC/layer_control/layerControlGrouped2.js
@@ -749,5 +749,5 @@ function create_colorPicker() {
   // color_picker.style.zIndex = "110";
   // color_picker.style.visibility = "hidden";
 
-  return `<div id="color_picker" class="color_picker" style="visibility = hidden;" onmouseout="hidecolorPicker()"></div>`; //color_picker;
+  return `<div id="color_picker" class="color_picker" style="display:none" onmouseleave="hidecolorPicker()"></div>`; //color_picker;
 }
