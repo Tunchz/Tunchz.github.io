a096110414dff268f88ea0cc593c3b8df9e3a83a
Tunchz <46678653+Tunchz@users.noreply.github.com>
2021-11-14 00:00:23 +0700

    update

--- DB.Builder/js/FWTT.init.js
+++ DB.Builder/js/FWTT.init.js
@@ -2241,3 +2241,13 @@ $.extend(freeboard, jQuery.eventEmitter),
 
     }
     ();
+
+
+// Utility functions
+function numberWithCommas(x) {
+    x = x.toString();
+    var pattern = /(-?\d+)(\d{3})/;
+    while (pattern.test(x))
+        x = x.replace(pattern, "$1,$2");
+    return x;
+}
--- DB.Builder/js/FWTT.widget.text.sparkline.js
+++ DB.Builder/js/FWTT.widget.text.sparkline.js
@@ -1,11 +1,4 @@
 (function(){
-        function numberWithCommas(x) {
-            x = x.toString();
-            var pattern = /(-?\d+)(\d{3})/;
-            while (pattern.test(x))
-                x = x.replace(pattern, "$1,$2");
-            return x;
-        }
 
         function a(a, b, c) {
             var d = $(b).text();
