fbe8076667eda786b6f398ff2aab29fe5700a318
Tunchz <46678653+Tunchz@users.noreply.github.com>
2021-11-14 21:39:35 +0700

    Update FWTT.widget.indicator.js

--- DB.Builder/js/FWTT.widget.indicator.js
+++ DB.Builder/js/FWTT.widget.indicator.js
@@ -76,12 +76,19 @@
         //---------- 1x4---------------------------------------------------------------------------
         freeboard.addStyle(".i1x4", "margin:auto;border-radius:50%;width:12px;height:12px;border:2px solid #3d3d3d;float:left;background-color:#222;transform: translateY(-50%);"); 
         var indicator1x4Plugin = function(settings) {
-            // function update() {
-            //     g.toggleClass("on", i), i ? f.text(_.isUndefined(c) ? _.isUndefined(h.on_text) ? "" : h.on_text : c) : f.text(_.isUndefined(d) ? _.isUndefined(h.off_text) ? "" : h.off_text : d)
-            //     g.css({height: h.size, width: h.size})
-            //     // g.css({height: h.size, width: h.size, "background-color": "#FF9900", "box-shadow": "0px 0px 15px "+"#FFFFFF"})
-            //     f.css({"font-size": h.font_size+"px"})
-            // }
+            function update() {
+                maxWidth = ($(_self).width()/4);
+                (maxWidth)?maxWidth-=4:maxWidth = (_w-_g*2)/4-4;
+                if (_settings.size<1) (_settings.size = 1);
+                (_settings.size>maxWidth)?size = maxWidth:size=_settings.size;
+
+                margin = (maxWidth-size)/2;
+                var attr = {height:size, width: size, margin: "0 "+margin+"px"};
+                g1.css(attr),
+                g2.css(attr),
+                g3.css(attr),
+                g4.css(attr)
+            }
             var _self, maxWidth, size = 1, margin,
                 container = $('<div class="indicator-light-container" style="height:50%;"></div>'),
                 g1 = $('<div class="i1x4"></div>'),
@@ -98,29 +105,18 @@
                 $(_self).append(container)
             }, this.onSettingsChanged = function(newSettings) {
                 _settings = newSettings
-                maxWidth = ($(_self).width()/4);
-                (maxWidth)?maxWidth-=4:maxWidth = (_w-_g*2)/4-4;
-                // console.log("------> ",maxWidth);
-                if (_settings.size<1) (_settings.size = 1);
-                // if (_settings.size>maxWidth) (_settings.size = maxWidth);
-                (_settings.size>maxWidth)?size = maxWidth:size=_settings.size;
-
-                margin = (maxWidth-size)/2;
-                var attr = {height:size, width: size, margin: "0 "+margin+"px"};
-                g1.css(attr),
-                g2.css(attr),
-                g3.css(attr),
-                g4.css(attr)
+                update()
             }, this.onCalculatedValueChanged = function(settingName, newValue) {
                 var newAttr = {"background-color": newValue, "box-shadow": "0px 0px 15px " + newValue,"border-color":"#FDF1DF"};
-                settingName=='color1'&&isOn[0] && (g1.css(newAttr)), 
-                settingName=='color2'&&isOn[1] && (g2.css(newAttr)),
-                settingName=='color3'&&isOn[2] && (g3.css(newAttr)), 
-                settingName=='color4'&&isOn[3] && (g4.css(newAttr)),
+                settingName=='color1'&&isOn[0] && (newValue?g1.css(newAttr):g1.css(offAttr)), 
+                settingName=='color2'&&isOn[1] && (newValue?g2.css(newAttr):g2.css(offAttr)),
+                settingName=='color3'&&isOn[2] && (newValue?g3.css(newAttr):g3.css(offAttr)), 
+                settingName=='color4'&&isOn[3] && (newValue?g4.css(newAttr):g4.css(offAttr)),
                 settingName=='value1'&&(Boolean(newValue)&&_settings.color1? isOn[0] = !0:(isOn[0] = !1,g1.css(offAttr))), 
                 settingName=='value2'&&(Boolean(newValue)&&_settings.color2? isOn[1] = !0:(isOn[1] = !1,g2.css(offAttr))),
                 settingName=='value3'&&(Boolean(newValue)&&_settings.color3? isOn[2] = !0:(isOn[2] = !1,g3.css(offAttr))), 
-                settingName=='value4'&&(Boolean(newValue)&&_settings.color4? isOn[3] = !0:(isOn[3] = !1,g4.css(offAttr)))
+                settingName=='value4'&&(Boolean(newValue)&&_settings.color4? isOn[3] = !0:(isOn[3] = !1,g4.css(offAttr))),
+                update()
             }, this.onDispose = function() {}, this.getHeight = function() {
                 return 1
             }, this.onSettingsChanged(settings)
