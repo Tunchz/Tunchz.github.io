ef986c23686310e6b823aa00fe8d79c99808114e
Tunchz <46678653+Tunchz@users.noreply.github.com>
2021-11-17 00:38:41 +0700

    Update FWTT.init.js

--- DB.Builder/js/FWTT.init.js
+++ DB.Builder/js/FWTT.init.js
@@ -2162,3 +2162,21 @@ function numberWithCommas(x) {
     }
 
 }
+
+function toThaiDateString(date) {
+    let monthNames = [
+        "มกราคม", "กุมภาพันธ์", "มีนาคม", "เมษายน",
+        "พฤษภาคม", "มิถุนายน", "กรกฎาคม", "สิงหาคม.",
+        "กันยายน", "ตุลาคม", "พฤศจิกายน", "ธันวาคม"
+    ];
+
+    let year = date.getFullYear() + 543;
+    let month = monthNames[date.getMonth()];
+    let numOfDay = date.getDate();
+
+    let hour = date.getHours().toString().padStart(2, "0");
+    let minutes = date.getMinutes().toString().padStart(2, "0");
+    let second = date.getSeconds().toString().padStart(2, "0");
+    
+    return `${numOfDay} ${month} ${year} ` + `${hour}:${minutes}:${second} น.`;
+}
