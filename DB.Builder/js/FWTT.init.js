30e95ad0020245d5ea352e7ff31cd2e5d7304f15
Tunchz <46678653+Tunchz@users.noreply.github.com>
2021-11-18 11:47:46 +0700

    Update FWTT.init.js

--- DB.Builder/js/FWTT.init.js
+++ DB.Builder/js/FWTT.init.js
@@ -2193,3 +2193,35 @@ function toThaiDateString(d,type) {
             `${numOfDay} ${month} ${year} ${hour}:${minutes}:${second}`;
 
 }
+
+function fullscreenById(id) {
+      if (
+        document.fullscreenElement ||
+        document.webkitFullscreenElement ||
+        document.mozFullScreenElement ||
+        document.msFullscreenElement
+      ) {
+        if (document.exitFullscreen) {
+          document.exitFullscreen();
+        } else if (document.mozCancelFullScreen) {
+          document.mozCancelFullScreen();
+        } else if (document.webkitExitFullscreen) {
+          document.webkitExitFullscreen();
+        } else if (document.msExitFullscreen) {
+          document.msExitFullscreen();
+        }
+      } else {
+        //element = $('#container').get(0);
+        element = document.getElementById(id);
+        console.log(element);
+        if (element.requestFullscreen) {
+          element.requestFullscreen();
+        } else if (element.mozRequestFullScreen) {
+          element.mozRequestFullScreen();
+        } else if (element.webkitRequestFullscreen) {
+          element.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+        } else if (element.msRequestFullscreen) {
+          element.msRequestFullscreen();
+        }
+      }
+}
